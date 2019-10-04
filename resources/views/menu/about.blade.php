4ebcc7dc4e170e70d59d37d6267d295416407df3
PhyoHtetAung <phyohtetaung.dev.29@gmail.com>
2019-10-04 08:11:14 +0630

    Second Commit

--- resources/views/menu/about.blade.php
+++ resources/views/menu/about.blade.php
@@ -5,14 +5,16 @@
 <div class="container aboutContainer">
     <h1>About Us</h1>
     <div class="whoWeAre">
-        <h1>Who We Are</h1>
+        <h1>What we did</h1>
         <p>
-        We offer a whole bunch of software for various platform and business. 
-        We mainly focus on creating websites which are similar to window applications. 
-        In this food ordering websites (only for game shops), we provide the most straightforward way to promote customer services. 
-        Shop owners can subscribe to use E & E website. Social Media login, such as Facebook and Google Login, is included. 
-        E & E website is responsive to every platform or devices. As it is a window like application, it does not need any online payment systems which take far more steps to cash. 
-        We offer three different periods to the shop owner to subscribe (one month, six months and one year).
+            We offer a whole bunch of software for various platform and business. 
+            We mainly focus on creating websites which are similar to window applications. 
+            In this food ordering websites (only for game shops), we provide the most straightforward way to promote customer services. 
+            Shop owners can subscribe to use E & E website. Social Media login, such as Facebook and Google Login, is included. 
+            E & E website is responsive to every platform or devices. As it is a window like application, it does not need any online payment systems which take far more steps to cash.
+            Like any other websites, data storing and retrieving can be done quickly and effortlessly.
+            Moreover, it has notification features at the admin side in order to respond immediately as soon as customers order the items.
+            Due to the simple procedures, useful features, attractive design, and secure payment system, it has a great potential to develop in the future.
         </p>
     </div>
     <div class="whyE">
